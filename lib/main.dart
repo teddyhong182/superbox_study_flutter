b90fa228a018fc5c1358b5f75b5964f5f4dcb425
teddyhong182 <superbox@dtime.com>
2022-02-28 13:14:27 +0900

    레이아웃 응용

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // custom design 을 개발하고 싶어도 MaterialApp 으로 하자 (디자인만 사용하지 않으면 됨)
 
-    return MaterialApp(
-        // 상중하로 나눠주는 Scaffold() 위젯
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('메롱')
-          ),
-          body: Container(),
-          bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const[
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                ]
-              ),
-            ),
-          ),
-    ));
+    return Scaffold(
+      appBar: AppBar( title: Text('앱임')),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: double.infinity, height: 50, color: Colors.blue,
+        ),
+      ),
+    );
+
+
+
+    // return MaterialApp(
+    //     // 상중하로 나눠주는 Scaffold() 위젯
+    //     home: Scaffold(
+    //       appBar: AppBar(
+    //         title: Text('메롱')
+    //       ),
+    //       body: Container(),
+    //       bottomNavigationBar: BottomAppBar(
+    //         child: SizedBox(
+    //           height: 100,
+    //           child: Row(
+    //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //             children: const[
+    //                 Icon(Icons.star),
+    //                 Icon(Icons.star),
+    //                 Icon(Icons.star),
+    //             ]
+    //           ),
+    //         ),
+    //       ),
+    // ));
   }
 }
