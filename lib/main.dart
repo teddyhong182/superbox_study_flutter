c01c0bfe4bdff301de18738d19c270192c43b20e
teddyhong182 <superbox@dtime.com>
2022-02-28 14:01:21 +0900

    추가 위젯 앱바 위젯 컨트롤

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // custom design 을 개발하고 싶어도 MaterialApp 으로 하자 (디자인만 사용하지 않으면 됨)
 
-    return Scaffold(
-      appBar: AppBar( title: Text('앱임')),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Container(
-          width: double.infinity, height: 50, color: Colors.blue,
-        ),
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              actions: [
+                Icon(Icons.monitor_heart),
+                Icon(Icons.monitor_heart),
+              ],
+                leading: Icon(Icons.star),
+                title: Text('앱임'),
+            ),
+            body: SizedBox(
+                child: ElevatedButton(
+                  child: Text('글자'),
+                  onPressed: () {},
+                )
+                // child: TextButton(
+                //   child: Text('글자'),
+                //   onPressed: () {},
+                // )
 
+                // child: Text(
+                //   '안녕하세요.',
+                //   style: TextStyle(
+                //       fontSize: 30, color: Color.fromRGBO(255, 0, 0, 0.0)),
+                // ),
+                // child: Icon(Icons.star),
+                )));
 
+    // return MaterialApp(
+    //     home: Scaffold(
+    //         appBar: AppBar(title: Text('앱임')),
+    //         body: Align(
+    //           alignment: Alignment.topCenter,
+    //           child: Container(
+    //             width: double.infinity,
+    //             height: 50,
+    //             color: Colors.blue,
+    //           ),
+    //         )));
 
     // return MaterialApp(
     //     // 상중하로 나눠주는 Scaffold() 위젯
