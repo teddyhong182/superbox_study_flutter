230b36fde090818fe9bb1ce1436955f7f6a530fe
teddyhong182 <superbox@dtime.com>
2022-02-28 10:37:46 +0900

    레이아웃 기초 - 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,17 @@ class MyApp extends StatelessWidget {
     // custom design 을 개발하고 싶어도 MaterialApp 으로 하자 (디자인만 사용하지 않으면 됨)
 
     return MaterialApp(
-      // 상중하로 나눠주는 Scaffold() 위젯
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar( child: Text('text'),),
-      )
-    );
-
+        // 상중하로 나눠주는 Scaffold() 위젯
+        home: Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // flex 와 유사
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.star),
+          Icon(Icons.star),
+          Icon(Icons.star)
+        ], // warning 은 lint 에 추가하여 설정 해제 가능
+      ),
+    ));
   }
 }
