b4b8283fd9b73101fe09886a9f9de2aaf46a3992
Kiwon Hong <superbox@dtime.co>
2022-03-20 16:23:40 +0900

    STEP 2 하단 탭 구성

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var _index = 0; // 페이지 인덱스 0, 1, 2
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('복잡한 UI'),
+      ),
+      body: Center(
+        child: Text(
+          '$_index 페이지',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index; // 선택된 탭의 인덱스로 _index를 변경
+          });
+        },
+        currentIndex: _index, // 선택된 인덱스
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: '홈',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: '이용서비스',
+            icon: Icon(Icons.assignment),
+          ),
+          BottomNavigationBarItem(
+            label: '내 정보',
+            icon: Icon(Icons.account_circle),
+          )
+        ],
+      ),
+    );
   }
 }
 
