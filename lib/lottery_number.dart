768db33ef6ee8f2dffc2e7899b1b9454a69b9f23
Kiwon Hong <superbox@dtime.co>
2022-03-22 08:28:21 +0900

    로또 번호 생성기 만들기

---
+++ lib/lottery_number.dart
@@ -0,0 +1,17 @@
+import 'dart:collection';
+import 'dart:math' as math;
+
+
+void main() {
+  var rand = math.Random();
+
+  HashSet<int> lotteryNumber = HashSet();
+
+  while(lotteryNumber.length < 6) {
+    lotteryNumber.add(rand.nextInt(45));
+
+  }
+
+  print(lotteryNumber);
+
+}
